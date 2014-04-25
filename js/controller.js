af66d737d74916bc984f22254ba6280ba6b1848b
katherineimogene <katherineimogene@gmail.com>
2014-04-25 14:56:01 -0700

    get a random pokemon from the API, serve to user on page load, after submitting a guess, and on request

--- js/controller.js
+++ js/controller.js
@@ -1,19 +1,26 @@
-var pokeApp = angular.module('pokeApp', []);
+var pokeApp = angular.module('pokeApp', [])
 
 pokeApp.controller('pokeController', function($scope, $http) {
-    $scope.submit = function() {
-        if ($scope.text) {
-            $http.get('http://pokeapi.co/api/v1/pokemon/' + this.text + '/').
-            success(function(data) {
-                $scope.currentPokemon = data;
-            });
-        }
-    };
+    angular.element(document).ready(function(){
+        $scope.getPoke()
+    })
+
+    $scope.getPoke = function(){
+        var pokeId = Math.floor(Math.random() * 151) + 1
+        $http.get('http://pokeapi.co/api/v1/pokemon/' + pokeId+ '/').
+        success(function(data) {
+            $scope.currentPokemon = data
+        })
+    }
+
     $scope.guess = function() {
         if (this.text2 == $scope.currentPokemon.name) {
-            $scope.lastAnswer = "Correct!";
+            $scope.lastAnswer = "Correct!"
+            $scope.getPoke()
         } else {
-            $scope.lastAnswer = "Incorrect!";
+            $scope.lastAnswer = this.text2 + " is incorrect! It was.." + $scope.currentPokemon.name
+            $scope.getPoke()
         }
+        this.text2 = ""
     }
-});
+})
