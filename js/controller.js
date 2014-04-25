31fbf871814892d92922ec96f9f2a568d2c127e8
katherineimogene <katherineimogene@gmail.com>
2014-04-25 15:14:22 -0700

    catch current pokemon as 'last' to display when we get the next pokemon. Also creates and displays a correct count

--- js/controller.js
+++ js/controller.js
@@ -4,6 +4,7 @@ pokeApp.controller('pokeController', function($scope, $http) {
     angular.element(document).ready(function(){
         $scope.getPoke()
     })
+    $scope.correctCount = 0
 
     $scope.getPoke = function(){
         var pokeId = Math.floor(Math.random() * 151) + 1
@@ -14,13 +15,23 @@ pokeApp.controller('pokeController', function($scope, $http) {
     }
 
     $scope.guess = function() {
+        $scope.catchLastPokemon()
         if (this.text2 == $scope.currentPokemon.name) {
             $scope.lastAnswer = "Correct!"
+            $scope.updateCount()
             $scope.getPoke()
         } else {
             $scope.lastAnswer = this.text2 + " is incorrect! It was.." + $scope.currentPokemon.name
             $scope.getPoke()
         }
         this.text2 = ""
+    }
+
+    $scope.catchLastPokemon = function(){
+        $scope.caughtPokemon = $scope.currentPokemon
+    }
+
+    $scope.updateCount = function(){
+        $scope.correctCount += 1
     }
 })
