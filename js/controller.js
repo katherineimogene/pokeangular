d94140450e534843bda10a8b485aa7c3d354c6ea
lilkraftwerk <lucasquinnmathis@gmail.com>
2014-04-25 14:19:42 -0700

    pokemon working

--- js/controller.js
+++ js/controller.js
@@ -1,12 +1,19 @@
 var pokeApp = angular.module('pokeApp', []);
 
-pokeApp.controller('PhoneListCtrl', function ($scope) {
-  $scope.phones = [
-    {'name': 'Nexus S',
-     'snippet': 'Fast just got faster with Nexus S.'},
-    {'name': 'Motorola XOOM™ with Wi-Fi',
-     'snippet': 'The Next, Next Generation tablet.'},
-    {'name': 'MOTOROLA XOOM™',
-     'snippet': 'The Next, Next Generation tablet.'}
-  ];
+pokeApp.controller('pokeController', function($scope, $http) {
+    $scope.submit = function() {
+        if ($scope.text) {
+            $http.get('http://pokeapi.co/api/v1/pokemon/' + this.text + '/').
+            success(function(data) {
+                $scope.currentPokemon = data;
+            });
+        }
+    };
+    $scope.guess = function() {
+        if (this.text2 == $scope.currentPokemon.name) {
+            $scope.lastAnswer = "Correct!";
+        } else {
+            $scope.lastAnswer = "Incorrect!";
+        }
+    }
 });
